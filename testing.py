14d3aa62751a2c35e9e129cdbce94e429e69140e
DillsMaca <gogeta0306@gmail.com>
2026-03-24 12:42:45 +1300

    Update testing.py
    uploaded some more testing code

--- testing.py
+++ testing.py
@@ -23,3 +23,29 @@ if __name__ == '__main__':
     menu.mainloop(surface)
 
 
+
+import pygame
+import pygame_menu
+# Initialize Pygame
+pygame.init()
+# Screen dimensions
+WIDTH = 1080
+HEIGHT = 1920
+screen = pygame.display.set_mode((WIDTH, HEIGHT)) #
+pygame.display.set_caption("The Number Trials")
+
+# Colours (RGB)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+# I will define more colours when I need it
+
+# This controls the framerate and helps with concistency over all devices
+clock = pygame.time.Clock()
+FPS = 60
+
+# Font setup
+font = pygame.font.SysFont(None, 40)
+
+# game state value
+game_state = "START_SCREEN"
+
