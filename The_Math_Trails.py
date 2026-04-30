0a97bd1c836b928fbd1f63ef1b20e88bed18567d
DillsMaca <gogeta0306@gmail.com>
2026-05-01 11:51:42 +1200

    Update The_Math_Trails.py
    I fixed the background and also added a grid to help me decide where to insert the platform islands.

--- The_Math_Trails.py
+++ The_Math_Trails.py
@@ -1,30 +1,4 @@
-"""
-import pygame
-import pygame_menu
-# Initialize Pygame
-pygame.init()
-# Screen dimensions
-WIDTH = 1080
-HEIGHT = 1920
-screen = pygame.display.set_mode((WIDTH, HEIGHT)) #
-pygame.display.set_caption("The Number Trials")
 
-# Colours (RGB)
-BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
-# I will define more colours when I need it
-
-# This controls the framerate and helps with concistency over all devices
-clock = pygame.time.Clock()
-FPS = 60
-
-# Font setup
-font = pygame.font.SysFont(None, 40)
-
-# game state value
-game_state = "START_SCREEN"
-
-"""
 # Importing modules to start pygame
 import pygame
 from pygame.locals import *
@@ -39,23 +13,46 @@ screen_height = 1000
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("The Number Trials")
 
+#define game variables
+tile_size = 200
+
 
 #load images (character and backgrounds)
 bg_img = pygame.image.load('img/sky.png')
 # Scale image to fill the screen dimensions
 bg_img = pygame.transform.scale(bg_img, (screen_width, screen_height))
 
-#sun_img = pygame.image.load('img/sun.png')
+def draw_grid():
+    for line in range(0, 6):
+        pygame.draw.line(screen, (255, 255, 255), (0, line * tile_size), (screen_width, line * tile_size))
+        pygame.draw.line(screen, (255, 255, 255), (line * tile_size, 0), (line * tile_size, screen_height))
 
 
+#class World():
+    #def __init__(self, data):
+
+    #load images
+    #platform_img = pygame.image.load("img/platform.png")
+
+world_data = [
+[1, 1, 1, 1, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 1, 1, 1, 1],
+]
 #creating loop so the game keeps running
 run = True
 while run: # == True:
+
+    draw_grid()
     # adding a way to close the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
         screen.blit(bg_img, (0, 0))
-        #screen.blit(sun_img, (100, 100))
+
     pygame.display.update()
 pygame.quit()
+
+
